4d91c4f6f137a2e663115261051089c1c457d525
张益豪 <zhangyh@world-tech.com.cn>
2018-07-12 09:16:21 +0800

    no message

--- studyvue/src/components/Count.vue
+++ studyvue/src/components/Count.vue
@@ -6,21 +6,49 @@
             <button @click="$store.commit('add')" >+</button>
             <button @click="reduce">-</button>
         </p>
-        <p>
+        <div>
             <h4>Action</h4>
             <button @click="addAction" >+</button>
             <button @click="reduceAction">-</button>
-        </p>
+        </div>
+        <el-row>
+            <el-col :span="6">
+                <div class="grid-content bg-purple-dark">123</div>
+                <span class="el-icon-info"></span>
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-button round @click="btnclick" >嘿</el-button>
+        </el-row>
+        <ve-pie :data="chartData"></ve-pie>
     </div>
 </template>
 <script>
 import store from '@/vuex/store';
+// mapState: store中数据处理方便的方法；
+// mapMutations: store中处理数据的方法；
+// mapGetters: 过滤数据；
+// mapActions: 异步处理数据，最终调用mautation改变数据
 import {mapState,mapMutations,mapGetters, mapActions} from 'vuex';
 export default {
     data(){
         return{
             msg:'Hello Vuex',
+            chartData: {
+                columns: ['日期', '访问用户'],
+                rows: [
+                    { '日期': '1/1', '访问用户': 1393 },
+                    { '日期': '1/2', '访问用户': 3530 },
+                    { '日期': '1/3', '访问用户': 2923 },
+                    { '日期': '1/4', '访问用户': 1723 },
+                    { '日期': '1/5', '访问用户': 3792 },
+                    { '日期': '1/6', '访问用户': 4593 }
+                ]
+            }
         }
+    },
+    beforeMount(){
+        this.$store.state.info = true
     },
     // 怎样把store中的数据拿到组件中使用(访问数据对象的方法)；方法1
     // comnputed:{
@@ -43,8 +71,17 @@ export default {
     },
     methods:{
         ...mapMutations(['add','reduce']),
-        ...mapActions(['addAction','reduceAction'])
+        ...mapActions(['addAction','reduceAction']),
+        btnclick(){
+            this.reduceAction();
+        }
     },
     store
 }
 </script>
+<style>
+    .bg-purple-dark{
+        background: red;
+    }
+</style>
+
--- studyvue/src/main.js
+++ studyvue/src/main.js
@@ -3,11 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router/index'
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import VeLine from 'v-charts/lib/line.common'
+Vue.component('pie',VeLine)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 // router 必须在根实例的构造器上挂载
+Vue.use(ElementUI)
 new Vue({
   el: '#app',
   router,
